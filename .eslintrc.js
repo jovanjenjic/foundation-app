3ef51394facdca736223676180f14d757d669545
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2023-05-10 16:42:39 +0200

    Eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
--- src/features/employees/types/index.ts
+++ src/features/employees/types/index.ts
@@ -1,30 +1,29 @@
 export interface Employee {
+  _id: string;
+  deletedAt: null | Date;
+  isDeleted: boolean;
+  dateOfBirth: Date;
+  dateOfEmployment: Date;
+  homeAddress: {
+    addressLine1: string;
+    addressLine2: string;
+    ZIPCode: string;
+    city: string;
     _id: string;
-    deletedAt: null | Date;
-    isDeleted: boolean;
-    dateOfBirth: Date;
-    dateOfEmployment: Date;
-    homeAddress: {
-      addressLine1: string;
-      addressLine2: string;
-      ZIPCode: string;
-      city: string;
-      _id: string;
-    };
-    phoneNumber: string;
-    email: string;
-    name: string;
-    __v: number;
-  }
+  };
+  phoneNumber: string;
+  email: string;
+  name: string;
+  __v: number;
+}
 
 export interface EmployeesResponse {
-    employees: Employee[];
-    count: number;
+  employees: Employee[];
+  count: number;
 }
 
 export interface EmployeeQueryArgsData {
-    page?: number;
-    size?: number;
-    searchText?: string;
-  };
-  
+  page?: number;
+  size?: number;
+  searchText?: string;
+}
