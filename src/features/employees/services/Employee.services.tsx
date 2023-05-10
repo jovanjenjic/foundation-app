a3d79f4bfe2d33d4a37e6c75ad5758a9ece78779
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2023-05-10 15:31:10 +0200

    Craco and path aliases

--- src/features/employees/services/Employee.services.tsx
+++ src/features/employees/services/Employee.services.tsx
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
-import { parseQueryString } from '../../../utils/parseQueryString';
+import { parseQueryString } from '@utils/parseQueryString';
 import { Employee, EmployeeQueryArgsData, EmployeesResponse } from '../types';
 
 const baseUrl = process.env.REACT_APP_SERVICE_URL;
